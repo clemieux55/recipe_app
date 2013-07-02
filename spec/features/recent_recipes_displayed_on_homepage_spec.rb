29fe81a3b23ce48a3b22dccf801164dd0c4c0193
Christopher Lemieux <clemieux598@gmail.com>
2013-07-02 16:59:40 -0400

    ingredients displayed with recent recipes on homepage

--- spec/features/recent_recipes_displayed_on_homepage_spec.rb
+++ spec/features/recent_recipes_displayed_on_homepage_spec.rb
@@ -1,32 +1,37 @@
 require 'spec_helper'
 
 describe 'homepage content' do
-
   let(:valid_user) { FactoryGirl.create(:user) }
-  
+  let(:ingredient1) { FactoryGirl.create(:ingredient) }
+  let(:ingredient2) { FactoryGirl.create(:ingredient) }
+  let(:ingredient3) { FactoryGirl.create(:ingredient) }
+
+  let!(:recipe1) { FactoryGirl.create(:recipe_with_ingredients, ingredients: [ingredient2, ingredient1, ingredient3]) }
+  let!(:recipe2) { FactoryGirl.create(:recipe_with_ingredients, ingredients: [ingredient2, ingredient1, ingredient3]) }
+  let!(:recipe3) { FactoryGirl.create(:recipe_with_ingredients, ingredients: [ingredient2, ingredient1, ingredient3]) }
+
   before :each do 
     visit root_path
     sign_in_as valid_user
   end
 
+
   it 'should have a recent recipe field' do
-    expect(page).to have_content('Recently Added')
+    expect(page).to have_content('Last Three Recipes Added')
   end
 
-  it 'should have the last recipe created as content' do
-    last_recipe_created
-    expect(page).to have_content('Donut')
+  it 'should have the last three recipe titles created displayed' do 
+    expect(page).to have_content(recipe1.title, recipe2.title, recipe3.title)
   end
 
-  def last_recipe_created
-    Ingredient.create(name: 'dough')
-    visit root_path
-    click_on 'Add Recipe'
-    fill_in "recipe_title", with: 'Donut'
-    fill_in 'recipe_description', with: 'Glazed Donut'
-    select 'dough', from: 'Ingredients'
-    click_on 'Create Recipe'
-    root_path
+  it 'should have the descriptions displayed for the recipe' do 
+    expect(page).to have_content(recipe1.description, recipe2.description, recipe3.description)
+  end
+
+  it 'should have the ingredients for recipe displayed' do 
+    recipe1.ingredients.each do |ingredient|
+      expect(page).to have_content(ingredient.name)
+    end
   end
 end
 
