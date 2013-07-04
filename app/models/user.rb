72e1f9f5af57903600f177e0ab3062c38fd3dee4
Christopher Lemieux <clemieux598@gmail.com>
2013-07-04 00:25:57 -0400

    removed user attributes not needed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me, :provider, :uid
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
 
   validates_presence_of :email, :password, :password_confirmation
